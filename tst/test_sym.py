4c19149a8863443708b99b03d18299e9c6846506
nitesh31mishra <nitesh31mishra@gmail.com>
2022-09-05 19:51:06 -0400

    updated the test file for sym class

---
+++ tst/test_sym.py
@@ -0,0 +1,12 @@
+from src.sym import Sym
+from src.main import oo
+
+def test_sym():
+    sym = Sym()
+    for i, x in ["a", "a", "a", "a" , "b", "b", "c"]:
+        sym.add(x)
+    mode, entropy = sym.mid(), sym.div()
+    entropy = (1000*entropy)//1/1000
+    oo({"mid":mode, "div":entropy})
+    return mode == "a" and 1.37 <= entropy and entropy <= 1.38
+
